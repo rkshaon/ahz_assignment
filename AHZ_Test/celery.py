5e5f11137c3210b5538524b73a6fbeae1ee3774a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2025-04-10 16:40:57 +0600

    done: the project

--- AHZ_Test/celery.py
+++ AHZ_Test/celery.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
-# from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AHZ_Test.settings')
 app = Celery('AHZ_Test')
@@ -11,9 +10,9 @@ app = Celery('AHZ_Test')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.conf.beat_schedule = {
-    'run-every-1-minutes': {
+    'run-every-30-minutes': {
         'task': 'book_api.tasks.archive_books_checker',
-        'schedule': 60,
+        'schedule': 1800,
     },
 }
 
--- book_api/tasks.py
+++ book_api/tasks.py
@@ -7,14 +7,11 @@ from book_api.models import Book
 
 @shared_task
 def archive_books_checker():
-    print("Archive books checker task executed.", timezone.now())
-    # Get all books that are older than 30 days
-    books_to_archive = Book.objects.filter(
+    Book.objects.filter(
         is_archived=False,
-        created_at__lt=timezone.now() - timezone.timedelta(days=30)
+        published_date__lt=timezone.now() - timezone.timedelta(days=365*10)
+    ).update(
+        is_archived=True
     )
-    # Archive them
-    for book in books_to_archive:
-        book.is_archived = True
-        book.save()
+
     return
